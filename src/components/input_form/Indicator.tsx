3e261e9de20a5f1bee40820eaa0ea91763ae6b1b
Bird-Forest <127691003+Bird-Forest@users.noreply.github.com>
2024-10-14 10:42:48 +0300

    fixed button

--- src/components/input_form/Indicator.tsx
+++ src/components/input_form/Indicator.tsx
@@ -13,7 +13,11 @@ const Indicator: React.FC<IndicatorProps> = ({ item, isOpen, openField }) => {
     <div className={styles.indicator}>
       <img src={item.path} alt={item.title} className={styles.imgIndicator} />
       <p className={styles.textIndicator}>{item.title}</p>
-      <button onClick={() => openField(item)} className={styles.iconIndicator}>
+      <button
+        type="button"
+        onClick={() => openField(item)}
+        className={styles.iconIndicator}
+      >
         {isOpen ? (
           <img src={item.up} alt="arrow up" width={20} />
         ) : (
